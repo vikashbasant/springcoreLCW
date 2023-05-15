8c978d6b70eec1c897965f93c6ba6c2f65b15745
Vikash Basant <basantvikash360@gmail.com>
2023-05-15 18:51:43 +0530

    another way of passing value to it

--- src/main/java/org/springcore/Main.java
+++ src/main/java/org/springcore/Main.java
@@ -10,7 +10,11 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Hello world!");
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
         Student student1 = (Student) context.getBean("student1");
         System.out.println(student1);
+
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student2);
     }
 }
