a47a73bc5d57f9dd42c7a9d96eb799782c83c57f
Vikash Basant <basantvikash360@gmail.com>
2023-05-17 20:00:15 +0530

    constructor injection

--- src/main/java/org/springcore/ci/Person.java
+++ src/main/java/org/springcore/ci/Person.java
@@ -1,5 +1,10 @@
 package org.springcore.ci;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Person {
     private String name;
 
@@ -7,14 +12,27 @@ public class Person {
 
     private Certi certi;
 
-    Person() {
+    private List<String> list;
 
-    }
-
-    Person(String name, int personId, Certi certi) {
+    public Person (String name, int personId, Certi certi, List<String> list, Set<String> set, Map<String, Integer> map, Properties prop) {
         this.name = name;
         this.personId = personId;
-        this.certi =  certi;
+        this.certi = certi;
+        this.list = list;
+        this.set = set;
+        this.map = map;
+        this.prop = prop;
+    }
+
+    private Set<String> set;
+
+    private Map<String, Integer> map;
+
+    private Properties prop;
+
+
+    Person() {
+
     }
 
     @Override
@@ -23,6 +41,19 @@ public class Person {
                 "name='" + name + '\'' +
                 ", personId=" + personId +
                 ", certi=" + certi +
+                ", list=" + list +
+                ", set=" + set +
+                ", map=" + map +
+                ", prop=" + prop +
                 '}';
     }
+
+    Person(String name, int personId, Certi certi, List<String> list, Set<String> set) {
+        this.name = name;
+        this.personId = personId;
+        this.certi =  certi;
+        this.list = list;
+        this.set = set;
+    }
+
 }
